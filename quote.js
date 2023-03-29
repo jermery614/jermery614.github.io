df518f8debbd2e30dde469035f02f008c4265b82
Jermery Wheeler <52702506+jermery614@users.noreply.github.com>
2023-03-29 15:39:11 -0400

    Updating quote.js adding functionality.

---
+++ quote.js
@@ -0,0 +1,23 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7f96213445mshc9fd0b9ea22e497p1807e8jsn4ef2e1d0ce8a',
+		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+	}
+};
+const dataFactsEL = document.querySelector('.quotes')
+
+
+fetch('https://famous-quotes4.p.rapidapi.com/random?category=happiness&count=1', options)
+	.then(response => response.json())
+	.then(response => {
+		for (let data of response){
+			console.log(data)
+			dataFactsEL.innerHTML = "Quote of the day: " +` --- ${data.text}` + " " +" "+ "---- "+ `${data.author}`;
+			
+		}
+	})
+	.catch(err => console.error(err));
+
+	//
+
