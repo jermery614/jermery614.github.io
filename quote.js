77ea9d8d867e8d3d8abf8afff2b3a1ea93a8275f
Jermery Wheeler <52702506+jermery614@users.noreply.github.com>
2023-03-30 22:39:07 -0400

    Update quote.js

--- quote.js
+++ quote.js
@@ -2,20 +2,18 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '7f96213445mshc9fd0b9ea22e497p1807e8jsn4ef2e1d0ce8a',
-		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
 	}
 };
 const dataFactsEL = document.querySelector('.quotes')
 
 
-fetch('https://famous-quotes4.p.rapidapi.com/random?category=happiness&count=1', options)
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
 	.then(response => response.json())
+	// .then(response => console.log(response))
 	.then(response => {
-		for (let data of response){
-			console.log(data)
-			dataFactsEL.innerHTML = "Quote of the day: " +` --- ${data.text}` + " " +" "+ "---- "+ `By: ${data.author}`;
-			
-		}
+		dataFactsEL.innerHTML="Quote: "+`  ${response.content}`+" "+" "+ "- "+ `By: ${response.originator.name}`;
+		
 	})
 	.catch(err => console.error(err));
 
